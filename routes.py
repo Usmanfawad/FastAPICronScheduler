7123e16c088c5c9c2972c502a7f48745a2f1cbe5
Usman Fawad <ufawad0@gmail.com>
2023-06-08 20:51:00 +0500

    FastAPI CRON scheduler

---
+++ routes.py
@@ -0,0 +1,20 @@
+from main import app
+
+from fastapi_utils.tasks import repeat_every
+from fastapi_utils.session import FastAPISessionMaker
+
+import requests
+import schedule
+
+
+@app.get("/")
+@app.on_event("startup")
+@repeat_every(seconds=5)
+async def root():
+    print("Root route initiated!")
+    url = 'http://127.0.0.1:5000/scraper/test'
+    x = requests.post(url)
+    print("posted")
+    print(x.json())
+    return {"message": x.json()}
+
