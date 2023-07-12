c8739dc0466520ef5b39a105772b5bba9f6b0959
Usman Fawad <ufawad0@gmail.com>
2023-07-12 23:39:27 +0500

    FastAPI CRON scheduler

--- ultraMessage.py
+++ ultraMessage.py
@@ -5,7 +5,7 @@ import ssl
 def send_message(destination_number, body):
     print("Sending message")
     CONN = http.client.HTTPSConnection("api.ultramsg.com", context=ssl._create_unverified_context())
-    payload = f"token=p25i2oy53uwkyte4&to=+17023497673&body={body}"
+    payload = f"token=p25i2oy53uwkyte4&to=+923352839515&body={body}"
     payload = payload.encode('utf8').decode('iso-8859-1')
     headers = {'content-type': "application/x-www-form-urlencoded"}
     CONN.request("POST", "/instance53326/messages/chat", payload, headers)
